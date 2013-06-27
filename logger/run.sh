4270757766a22fefbb89673a3169fce281b029e9
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2013-06-28 00:57:01 +0300

    Helpers

---
+++ logger/run.sh
@@ -0,0 +1,13 @@
+python all.py
+python set.py 1
+python set.py 2
+python set.py 3
+python set.py 4
+python set.py 5
+python set.py 6
+python set.py 7
+python set.py 8
+python set.py 9
+python set.py 10
+python set.py 11
+python set.py 12
